f1624b9f065e311f49363d0d3a6c258c9e123c16
George Hart <George.hart@conferma.com>
2021-02-16 09:51:07 +0000

    Tidy up comment

--- app/src/main/java/com/example/faceitstatchecker/Network/FaceitDataInterface.kt
+++ app/src/main/java/com/example/faceitstatchecker/Network/FaceitDataInterface.kt
@@ -9,10 +9,6 @@ import retrofit2.http.Query
  * Created by George Hart on 15/02/2021;
  */
 interface FaceitDataInterface {
-    /*
-    *  @POST ("AddNotes")
-    Call<AddNotesResponse> addNotes(@Body AddNotesRequest request);
-    * */
     @GET("/search/players")
     fun playerSearch(@Query("nickname") playerName: String) : Call<PlayerSearchResponse>
 }
